6430e61ee2429c7b8f562d55f96a544640990795
Aaron Lecourt <20217375@s.ubaguio.edu>
2023-11-23 20:19:40 +0800

    commit fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,3 @@
-import EducBg from "./component/EducBg";
-import Contentcontain from "./component/Contentcontain";
 import HomeDetails from "./component/HomeDetails";
 import useFetch from "./useFetch";
 import ReviewList from "./ReviewList";
@@ -13,7 +11,7 @@ const Home = () => {
         <div className="px-6 overflow-y-auto h-screen w-100 grid grid-cols-7 max-lg:grid-cols-1">
             <div className="col-span-2 border-r border-slate-100/10 pr-3 text-white xl:col-span-1 max-lg:hidden">
             {error && <div className="font-valorant"> {error} </div>}
-                {reviews == 0 && <div className="font-valorant text-sm text-slate-500/50"> No Reviews </div>}
+                {reviews === 0 && <div className="font-valorant text-sm text-slate-500/50"> No Reviews </div>}
                 {isPending && <div className="font-valorant text-sm text-slate-500/50"> Loading Reviews... </div>}
                 {reviews && <ReviewList reviews = {reviews} reviewlist = "" />}
             </div>
